d1c184c850e1dcc2a8acd42d655a82637aa72136
BrendenSeifried <brendenseifried@gmail.com>
2022-04-28 15:37:03 -0700

    array showing forgot.results in the fetch

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -2,5 +2,5 @@ export async function fetchRnM() {
   const response = await fetch('https://rickandmortyapi.com/api/character');
   const rickandm = await response.json();
   console.log(rickandm);
-  return rickandm;
+  return rickandm.results;
 }
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,14 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchRnM } from '../services/fetch';
 
 export default function Main() {
+  const [char, setChar] = useState([]);
+
   useEffect(() => {
     const fetchApi = async () => {
-      const test = await fetchRnM();
-      //   setInfo(allApi);
-      console.log(test);
+      const results = await fetchRnM();
+      // console.log(allChar);
+      setChar(results);
     };
     fetchApi();
   }, []);
-  return <div>Main</div>;
+
+  return (
+    <>
+      {char.map((item) => (
+        <article key={item.id}>
+          <div>
+            <h1>Name: {item.name}</h1>
+            <p>Status: ({item.status})</p>
+          </div>
+        </article>
+      ))}
+    </>
+  );
 }
