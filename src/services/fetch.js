a7e5415d8f4b9b95d2b125d61a6216ccb9c6ac96
BrendenSeifried <brendenseifried@gmail.com>
2022-04-28 17:06:05 -0700

    added alive parameter to url as a test for filtering

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,5 +1,7 @@
-export async function fetchRnM() {
-  const response = await fetch('https://rickandmortyapi.com/api/character');
+export async function fetchRnM(setLiving) {
+  const response = await fetch(
+    'https://rickandmortyapi.com/api/character?status=alive'
+  );
   const rickandm = await response.json();
   console.log(rickandm);
   return rickandm.results;
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -3,11 +3,11 @@ import { fetchRnM } from '../services/fetch';
 
 export default function Main() {
   const [char, setChar] = useState([]);
+  const [living, setLiving] = useState('');
 
   useEffect(() => {
     const fetchApi = async () => {
       const results = await fetchRnM();
-      // console.log(allChar);
       setChar(results);
     };
     fetchApi();
@@ -15,14 +15,17 @@ export default function Main() {
 
   return (
     <>
-      {char.map((item) => (
-        <article key={item.id}>
-          <div>
-            <h1>Name: {item.name}</h1>
-            <p>Status: ({item.status})</p>
-          </div>
-        </article>
-      ))}
+      <div>{/* <Order setOrder={setOrder} /> */}</div>
+      <>
+        {char.map((item) => (
+          <article key={item.id}>
+            <div>
+              <h1>Name: {item.name}</h1>
+              <p>Status: ({item.status})</p>
+            </div>
+          </article>
+        ))}
+      </>
     </>
   );
 }
